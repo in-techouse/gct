76c081c0bd2ab77c154345a3f81163050b25004a
Sana <sanaabid74@gmail.com>
2020-01-10 10:28:59 +0500

    firebase edit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,19 @@
 var express = require('express');
+var firebase = require('firebase');
 var router = express.Router();
+
+var firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
 
 /* GET home page. */
 router.get('/', function(req, res) {
