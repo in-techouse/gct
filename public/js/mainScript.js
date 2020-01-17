2ed0c8d97e9b14fa2b871f9ead647135256e28cc
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2020-01-17 09:38:37 +0500

    Working on Social Logins

--- public/js/mainScript.js
+++ public/js/mainScript.js
@@ -1,17 +1,50 @@
-$(document).ready(function(){
-    console.log('Main Script Document is ready');
-    setTimeout(function(){
-        console.log('Time out called');
-        $('#hellopreloader').fadeOut(1000);
-    }, 1000);
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAFSBuiVUoFC18qzI3xd3EsGL_Fsy4y5VA",
+  authDomain: "global-ct.firebaseapp.com",
+  databaseURL: "https://global-ct.firebaseio.com",
+  projectId: "global-ct",
+  storageBucket: "global-ct.appspot.com",
+  messagingSenderId: "559718967029",
+  appId: "1:559718967029:web:6cd2b538b1fe3d940dc7bc",
+  measurementId: "G-H4442SC9K6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-    $('#facebookLogin').click(function(){
-    	console.log('Clicked');
-		  var provider = new firebase.auth.FacebookAuthProvider();
-		  firebase.auth().signInWithPopup(provider).then(r=>{
-		  	console.log('Success: ', r);
-		  }).catch(e=>{
-		  	console.log('Error: ', e);
-		  })
-    });
+$(document).ready(function() {
+  console.log("Main Script Document is ready");
+  setTimeout(function() {
+    console.log("Time out called");
+    $("#hellopreloader").fadeOut(1000);
+  }, 1000);
+
+  $("#facebookLogin").click(function() {
+    console.log("Clicked");
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(r => {
+        console.log("Success: ", r);
+      })
+      .catch(e => {
+        console.log("Error: ", e);
+      });
+  });
+
+  $("#twitterLogin").click(function() {
+    console.log("Clicked");
+    var provider = new firebase.auth.TwitterAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(r => {
+        console.log("Success: ", r);
+      })
+      .catch(e => {
+        console.log("Error: ", e);
+      });
+  });
 });
