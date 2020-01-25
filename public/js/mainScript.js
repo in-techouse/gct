e7f6d9882b4a09ff0d07266ed8bfdbcf6a84afc6
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2020-01-25 11:09:46 +0500

    Hopefully Fixed

--- public/js/mainScript.js
+++ public/js/mainScript.js
@@ -13,118 +13,139 @@ firebase.analytics();
 
 function loginwithTwitter(d) {
   $.ajax({
-    url: '/twitterLogin',
-    type: 'POST',
+    url: "/twitterLogin",
+    type: "POST",
     data: {
       accessToken: d.credential.accessToken,
-      secret: d.credential.secret,
+      secret: d.credential.secret
     },
-    success: function (data) {
-      console.log('Success: ', data);
-      if(data == "-1"){
-        $('#upperTwitter').show(300);
-        $('#mainError').show(300);
-      }
-      else if (data == "2"){
+    success: function(data) {
+      console.log("Success: ", data);
+      if (data == "-1") {
+        $("#upperTwitter").show(300);
+        $("#mainError").show(300);
+      } else if (data == "2") {
         // window.location.reload();
-        $('#username').show(300);
-      }
-      else if (data == "3"){
+        $("#username").show(300);
+      } else if (data == "3") {
         window.location.reload();
       }
-      $('#loading').hide(300);
-    $('#mainAuth').show(300);
+      $("#loading").hide(300);
+      $("#mainAuth").show(300);
     },
-    error: function (error) {
-      console.log('Error:', error);
-      $('#upperTwitter').show(300);
-      $('#mainError').show(300);
+    error: function(error) {
+      console.log("Error:", error);
+      $("#upperTwitter").show(300);
+      $("#mainError").show(300);
     }
   });
 }
 
 function loginWithFacebook(d) {
   $.ajax({
-    url: '/facebookLogin',
-    type: 'POST',
+    url: "/facebookLogin",
+    type: "POST",
     data: {
-      accessToken: d.credential.accessToken,
+      accessToken: d.credential.accessToken
     },
-    success: function (data) {
-      console.log('Success: ', data);
-      if(data == "-1"){
-        $('#upperFacebook').show(300);
-        $('#mainError').show(300);
-      }
-      else if (data == "2"){
+    success: function(data) {
+      console.log("Success: ", data);
+      if (data == "-1") {
+        $("#upperFacebook").show(300);
+        $("#mainError").show(300);
+      } else if (data == "2") {
         window.location.reload();
-        $('#username').show(300);
-      }
-      else if (data == "3"){
+        $("#username").show(300);
+      } else if (data == "3") {
         window.location.reload();
       }
-      $('#loading').hide(300);
-    $('#mainAuth').show(300);
+      $("#loading").hide(300);
+      $("#mainAuth").show(300);
     },
-    error: function (error) {
-      console.log('Error:', error);
-      $('#mainError').show(300);
+    error: function(error) {
+      console.log("Error:", error);
+      $("#mainError").show(300);
     }
   });
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
   console.log("Main Script Document is ready");
-  setTimeout(function () {
+  setTimeout(function() {
     console.log("Time out called");
     $("#hellopreloader").fadeOut(1000);
   }, 1000);
 
-  $("#facebookLogin").click(function () {
+  $("#facebookLogin").click(function() {
     console.log("Clicked");
-    $('#mainError').hide(300);
-    $('#loading').show(300);
-    $('#mainAuth').hide(300);
+    $("#mainError").hide(300);
+    $("#loading").show(300);
+    $("#mainAuth").hide(300);
+    let signIn = 1;
+    if(firebase.auth().currentUser === null){
+      signIn = 0;
+    }
     var provider = new firebase.auth.FacebookAuthProvider();
     firebase
       .auth()
       .signInWithPopup(provider)
       .then(r => {
         console.log("Success: ", r);
-        $('#upperFacebook').hide(300);
-        firebase.auth().currentUser.delete().then(d=>{
+        $("#upperFacebook").hide(300);
+        if(signIn === 1){
+          firebase
+          .auth()
+          .currentUser.delete()
+          .then(d => {
+            loginWithFacebook(r);
+          });
+        }
+        else{
           loginWithFacebook(r);
-        });
+        }
+
       })
       .catch(e => {
         console.log("Error: ", e);
-        $('#mainError').show(300);
-        $('#loading').hide(300);
-        $('#mainAuth').show(300);
+        $("#mainError").show(300);
+        $("#loading").hide(300);
+        $("#mainAuth").show(300);
       });
   });
 
-  $("#twitterLogin").click(function () {
+  $("#twitterLogin").click(function() {
     console.log("Clicked");
-    $('#mainError').hide(300);
-    $('#loading').show(300);
-    $('#mainAuth').hide(300);
+    $("#mainError").hide(300);
+    $("#loading").show(300);
+    $("#mainAuth").hide(300);
+    let signIn = 1;
+    if(firebase.auth().currentUser === null){
+      signIn = 0;
+    }
     var provider = new firebase.auth.TwitterAuthProvider();
     firebase
       .auth()
       .signInWithPopup(provider)
       .then(r => {
         console.log("Success: ", r);
-        $('#upperTwitter').hide(300);
-        firebase.auth().currentUser.delete().then(d=>{
-          loginwithTwitter(r);
-        });
+        $("#upperTwitter").hide(300);
+        if(signIn === 1){
+          firebase
+          .auth()
+          .currentUser.delete()
+          .then(d => {
+            loginwithTwitter(r);
+          });
+        }
+        else{
+          loginwithTwitter(r); 
+        }
       })
       .catch(e => {
         console.log("Twitter Error: ", e);
-        $('#mainError').show(300);
-        $('#loading').hide(300);
-    $('#mainAuth').show(300);
+        $("#mainError").show(300);
+        $("#loading").hide(300);
+        $("#mainAuth").show(300);
       });
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -15,37 +15,66 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+router.get("/user", function(req, res) {
+  firebase
+    .database()
+    .ref()
+    .child("Users")
+    .child("PEIMndySWkdAuPD6IRbP1MIIpJ02")
+    .once("value")
+    .then(r => {
+      if (r.val() == null || r.val() == undefined) {
+        res.json("-1");
+      } else {
+        res.json("1");
+      }
+    })
+    .catch(e => {
+      res.json(e);
+    });
+  // if (req.session.isLoggedIn == true) {
+  //   res.redirect("/user/newsfeed");
+  // } else {
+  //   res.render("pages/index");
+  // }
+});
+
 /* GET home page. */
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
   if (req.session.isLoggedIn == true) {
     res.redirect("/user/newsfeed");
   } else {
     res.render("pages/index");
   }
 });
-router.post("/username", function (req, res) {
+router.post("/username", function(req, res) {
   let id = firebase.auth().currentUser.uid;
   let img = firebase.auth().currentUser.photoURL;
   let user = {
     firstName: req.body.firstname,
     lastName: req.body.lastname,
     image: img,
-    id: id,
+    id: id
   };
-  firebase.database().ref().child("Users").child(user.id).set(user).then(r => {
-    req.session.firstName = user.firstName;
-    req.session.lastName = user.lastName;
-    req.session.img = user.img;
-    req.session.id = id;
-    res.redirect('/user/newsfeed');
-  })
+  firebase
+    .database()
+    .ref()
+    .child("Users")
+    .child(user.id)
+    .set(user)
+    .then(r => {
+      req.session.firstName = user.firstName;
+      req.session.lastName = user.lastName;
+      req.session.img = user.img;
+      req.session.id = id;
+      res.redirect("/user/newsfeed");
+    })
     .catch(e => {
-      res.render('pages/index');
+      res.render("pages/index");
     });
-
 });
 
-router.post("/facebookLogin", function (req, res) {
+router.post("/facebookLogin", function(req, res) {
   var credential = firebase.auth.FacebookAuthProvider.credential(
     req.body.accessToken
   );
@@ -67,26 +96,29 @@ router.post("/facebookLogin", function (req, res) {
       .signInWithCredential(credential)
       .then(result => {
         // Chec user record in database
-        firebase.database.child("Users").child(firebase.auth().currentUser.uid).once('value').then(r => {
-          if (r == null || r == undefined || r.val() == null || r.val() == undefined) {
-            req.session.facebook = true;
-            res.json("1");
-
-          }
-          else {
-            req.session.isLoggedIn = true;
-            req.session.firstName = r.val().firstName;
-            req.session.lastName = r.val().lastName;
-            req.session.img = r.val().img;
-            req.session.id = r.val().id;
-            res.json("3");
-          }
-        })
+        firebase
+          .database()
+          .ref()
+          .child("Users")
+          .child(firebase.auth().currentUser.uid)
+          .once("value")
+          .then(r => {
+            if (r.val() == null || r.val() == undefined) {
+              req.session.facebook = true;
+              res.json("1");
+            } else {
+              req.session.isLoggedIn = true;
+              req.session.firstName = r.val().firstName;
+              req.session.lastName = r.val().lastName;
+              req.session.img = r.val().image;
+              req.session.id = r.val().id;
+              res.json("3");
+            }
+          })
           .catch(e => {
             req.session.facebook = true;
             res.json("1");
           });
-
       })
       .catch(e => {
         res.json(e.message);
@@ -94,7 +126,7 @@ router.post("/facebookLogin", function (req, res) {
   }
 });
 
-router.post("/twitterLogin", function (req, res) {
+router.post("/twitterLogin", function(req, res) {
   var credential = firebase.auth.TwitterAuthProvider.credential(
     req.body.accessToken,
     req.body.secret
@@ -117,26 +149,29 @@ router.post("/twitterLogin", function (req, res) {
       .signInWithCredential(credential)
       .then(result => {
         // Chec user record in database
-        firebase.database.child("Users").child(firebase.auth().currentUser.uid).once('value').then(r => {
-          if (r == null || r == undefined || r.val() == null || r.val() == undefined) {
-            req.session.twitter = true;
-            res.json("1");
-
-          }
-          else {
-            req.session.isLoggedIn = true;
-            req.session.firstName = r.val().firstName;
-            req.session.lastName = r.val().lastName;
-            req.session.img = r.val().img;
-            req.session.id = r.val().id;
-            res.json("3");
-          }
-        })
+        firebase
+          .database()
+          .ref()
+          .child("Users")
+          .child(firebase.auth().currentUser.uid)
+          .once("value")
+          .then(r => {
+            if (r.val() == null || r.val() == undefined) {
+              req.session.twitter = true;
+              res.json("1");
+            } else {
+              req.session.isLoggedIn = true;
+              req.session.firstName = r.val().firstName;
+              req.session.lastName = r.val().lastName;
+              req.session.img = r.val().image;
+              req.session.id = r.val().id;
+              res.json("3");
+            }
+          })
           .catch(e => {
             req.session.twitter = true;
             res.json("1");
           });
-
       })
       .catch(e => {
         res.json(e.message);
