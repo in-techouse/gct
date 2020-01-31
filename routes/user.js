c6513bf594c973bc6d823654f5ce90919d42cfae
ayesha <ashcoudhry@gmail.com>
2020-01-31 10:38:55 +0500

    Some minor fixes

--- routes/user.js
+++ routes/user.js
@@ -3,44 +3,44 @@ var firebase = require('firebase');
 var router = express.Router();
 
 
-router.get('/newsfeed', function(req, res) {
-  if(req.session.isLoggedIn){
+router.get('/newsfeed', function (req, res) {
+  if (req.session.isLoggedIn) {
     let user = req.session;
-    res.render('pages/user/dashboard', {user: user});
+    res.render('pages/user/dashboard', { user: user });
   }
-  else{
+  else {
     res.redirect("/");
   }
 });
 
-router.get('/profile', function(req, res) {
-  if(req.session.isLoggedIn){
+router.get('/profile', function (req, res) {
+  if (req.session.isLoggedIn) {
     let user = req.session;
-    res.render('pages/user/profile', { user: user});
+    res.render('pages/user/profile', { user: user });
   }
-  else{
+  else {
     res.redirect("/");
   }
 });
 
-router.get('/profilesettings', function(req, res) {
-  // if(req.session.isLoggedIn){
-  //   let user = req.session;
-    res.render('pages/user/profilesettings', { user: user});
-  // }
-  // else{
-  //   res.redirect("/");
-  // }
+router.get('/profilesettings', function (req, res) {
+  if (req.session.isLoggedIn) {
+    let user = req.session;
+    res.render('pages/user/profilesettings', { user: user });
+  }
+  else {
+    res.redirect("/");
+  }
 });
 
-router.get('/profileabout', function(req, res) {
-  // if(req.session.isLoggedIn){
+router.get('/profileabout', function (req, res) {
+  if (req.session.isLoggedIn) {
     let user = req.session;
-    res.render('pages/user/profileabout', { user: user});
-  // }
-  // else{
-  //   res.redirect("/");
-  // }
+    res.render('pages/user/profileabout', { user: user });
+  }
+  else {
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
