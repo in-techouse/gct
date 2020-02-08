ef1e2c035eecc519120d9f6068fecb1d834f01a3
ayesha <ashcoudhry@gmail.com>
2020-02-08 11:04:37 +0500

    working on tweets

--- public/js/mainScript.js
+++ public/js/mainScript.js
@@ -149,3 +149,5 @@ $(document).ready(function() {
       });
   });
 });
+
+
---
+++ public/js/newsfeed.js
@@ -0,0 +1,33 @@
+function gettweets() {
+    $.ajax({
+        url: "/user/tweets",
+        type: "GET",
+
+        success: function (data) {
+            console.log("Success: ", data);
+            if (data!== "-1"){
+                console.log("tweets: ", data.t);
+                let tweets=data.t;
+                tweets.forEach(tweet  => {
+                    console.log("tweet: " , tweet);
+                    
+                });
+            }
+            else{
+                window.location.reload();
+            }
+
+        },
+        error: function (error) {
+            console.log("Error:", error);
+
+        }
+    });
+
+
+}
+
+$(document).ready(function () {
+    console.log("newsfeed document is ready");
+    gettweets();
+});
