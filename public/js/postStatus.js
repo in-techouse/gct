2275168c47455ed9f453a1ce8c557afb4375a150
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2020-05-18 15:12:53 +0500

    Font AweSome Icons Added

--- public/js/postStatus.js
+++ public/js/postStatus.js
@@ -5,6 +5,10 @@ $(document).ready(function () {
     $("#photoUpload").trigger("click");
   });
 
+  $("#photoUpload").change(function () {
+    readURL(this);
+  });
+
   $("#postStatus").submit(function (event) {
     event.preventDefault();
     console.log("Form is stopped.");
@@ -23,3 +27,17 @@ $(document).ready(function () {
     // });
   });
 });
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $("#postImage").attr("src", e.target.result);
+      $("#imageUpper").show(400);
+      // $("#showDesign").addClass("img-thumbnail");
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
